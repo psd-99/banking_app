34dcbde6cacc2232337f324b03cc7672e0bbca91
psd-99 <pasinduekanayaka99@gmail.com>
2024-08-20 23:47:34 +0530

    modify the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import { HeaderBox } from "@/components/HeaderBox";
+import { TotalBalanceBox } from "@/components/TotalBalanceBox";
 import React  from "react";
 
 const Home = () => {
+
+const loggedIn = {firstName: 'Pasindu'};
+
     return(
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <div className="home-header">
+                    <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || "Guest"}
+                    subtext="Access and manage your accountand transactions efficiently"
+                    />
+
+                    <TotalBalanceBox 
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.54}
+                    />
+                </div>
+            </div>
+        </section>
     )
 }
 
